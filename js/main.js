c454051b4bc5169c219c9c5636a904790f9ea5db
Anirban.D <sarfaraz.k@niveussolutions.com>
2022-12-14 17:05:22 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+
+$(document).ready(function(){
+  
+  $('.service_slider').slick({
+    dots: false,
+    arrows: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplaySpeed: 1400,
+    fade: true,
+    cssEase: 'linear',
+  });
+});
